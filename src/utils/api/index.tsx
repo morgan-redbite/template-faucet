1337d62239addd51dbbd3e560377a604d1fff62f
mlyk1234 <32647481+mlyk1234@users.noreply.github.com>
2022-12-12 05:55:48 +0800

    change REACT_APP to GATSBY

--- src/utils/api/index.tsx
+++ src/utils/api/index.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.REACT_APP_API_URL || "http://localhost:8080";
+axios.defaults.baseURL = process.env.GATSBY_API_URL || "http://localhost:8080";
 
 export const getJWT = async () => {
   const jwt = localStorage.getItem("access_token");
